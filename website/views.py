3084e3f9d94de6594a23398f4d9d86d5773f2ee7
asayda01 <ahmetcemsaydam@gmail.com>
2025-01-19 13:41:34 +0000

    bolum 6 tamamlandi

--- website/views.py
+++ website/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def home(request):
-    return render(request, 'home.html', {})
+
+    # Check to see if logging in
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been login successfully")
+            return redirect("home")
+
+        else:
+            messages.success(request, "There was an error while logging in try agian")
+            return redirect("home")
+
+    else:
+        return render(request, 'home.html', {})
+
+
+def login_user(request):
+    pass
+
+
+def logot_user(request):
+    pass
