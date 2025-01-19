a99f18934792d492b5bdee2ce13dcf412516001e
asayda01 <ahmetcemsaydam@gmail.com>
2025-01-19 11:16:07 +0000

    initial commit

---
+++ mydb.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+try:
+    dataBase = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        passwd='11111'
+    )
+    if dataBase.is_connected():
+        print("Connection successful")
+
+except Error as e:
+    print(f"Error: {e}")
+
+# Prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# Create a DataBase
+cursorObject.execute("CREATE DATABASE elderco")
+
+print("All Done!!!")
