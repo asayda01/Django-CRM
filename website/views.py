ea303a745eb43951ecb414515a0b14b6f19ebaca
asayda01 <ahmetcemsaydam@gmail.com>
2025-01-19 20:17:09 +0000

    registration section completed

--- website/views.py
+++ website/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 
 def home(request):
-
     # Check to see if logging in
 
     if request.method == "POST":
@@ -36,3 +36,26 @@ def logout_user(request):
     messages.success(request, "You have been log OUT successfully")
     return redirect("home")
 
+
+def register_user(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            # Authenticate and then Login
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+
+            user = authenticate(username=username, password=password)
+
+            # Login
+            login(request, user)
+            messages.success(request, 'You have successfully registered ! ! !')
+            return redirect("home")
+
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+
+    return render(request, 'register.html', {'form': form})
